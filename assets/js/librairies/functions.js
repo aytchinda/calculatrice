03cc72d8f31b375f37562c07db26847f2fb7f7ad
Aytchinda <aymardrinel@outlook.fr>
2025-01-30 13:31:11 +0100

    modification initialisation des evements

--- assets/js/librairies/functions.js
+++ assets/js/librairies/functions.js
@@ -1,7 +1,30 @@
+
+const elements =  document.querySelectorAll(".col-calculette-1 .line-calculette > *");
+    const screen = elements[0];
+    const cancel = elements[1];
+    const del = elements[2];
 export const CalculetteObject = {
 
     initEvent: (touches) => {
-        console.log(touches);
-        
+        cancel.addEventListener("click", CalculetteObject.initCancel); 
+        del.addEventListener("click", CalculetteObject.initDelete);       
+         if (touches.length) {
+            touches.forEach(touche => {
+                touche.addEventListener("click", () => {
+                    const value = touche.innerText;
+
+                    screen.value += value;
+
+                    console.log(value);
+                    
+                });
+            });
+         }
+    },
+    initCancel: () => {
+        screen.value = "";
+    },
+    initDelete: () => {
+        screen.value = screen.value.slice(0, -1);
     },
 }
