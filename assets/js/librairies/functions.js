201b6b899fda202398f2d3f2818390213de7f2fc
Aytchinda <aymardrinel@outlook.fr>
2025-01-30 13:17:30 +0100

    modification du code , initialisation des evements

--- assets/js/librairies/functions.js
+++ assets/js/librairies/functions.js
@@ -1,27 +1,7 @@
-
-
 export const CalculetteObject = {
 
-    initEvent: (screen, touches, cancel,del) => {
-        cancel.addEventListener("click", () => CalculetteObject.initCancel(screen)); 
-        del.addEventListener("click", () => CalculetteObject.initDelete(screen)); 
-            
+    initEvent: (touches) => {
+        console.log(touches);
         
-        if (touches.length) {
-            touches.forEach(touche => {
-                touche.addEventListener("click", () => {
-                    const value = touche.innerText;
-                    screen.value += value;
-                    console.log(value);
-                })
-            });
-        }
     },
-    initCancel: (screen) => {
-        screen.value = "";
-    },
-    initDelete: (screen) => {
-        screen.value = screen.value.substring(0, screen.value.length - 1);
-        }
-
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 import {CalculetteObject} from "./librairies/functions.js";
-
 window.onload = () => {
-    const elements  = document.querySelectorAll(".col-calculette-1 .line-calculette > *");
+   
+    const elements =  document.querySelectorAll(".col-calculette-1 .line-calculette > *");
     const screen = elements[0];
     const cancel = elements[1];
     const del = elements[2];
+
     const touches = ([].slice.call(elements)).slice(3);
     
-    CalculetteObject.initEvent(screen , touches, cancel,del);
-
+    CalculetteObject.initEvent (touches);
+    
 }
